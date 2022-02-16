9f5712b64d35afdc8ddd51a998d8babddf01ba5c
Amruth <ammumandappa@gmail.com>
2022-02-16 06:01:45 +0530

    Update 6_dictinory.py

--- 6_dictinory.py
+++ 6_dictinory.py
@@ -2,3 +2,8 @@
             # --- key in dictionary does not allow polymorphism 
 
 #--- Dictionary keys are case sensitive, the same name but different case of key will be  treated distinctly.
+
+#----> key:value     value in the dictionary can be of any data type and can be duplicate. where as keys must be immutable.
+
+dict = {1:"hello",2:"world", 3:69, 5:[1,2,3,4,5]}
+print(f"the newely created dictionary is \t{dict}")
