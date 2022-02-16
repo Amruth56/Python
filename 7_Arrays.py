6167c6cc5edff43d6df9cc79eea72e4a0fb7d8c0
Amruth <ammumandappa@gmail.com>
2022-02-16 23:09:59 +0530

    Create 7_Arrays.py

---
+++ 7_Arrays.py
@@ -0,0 +1,34 @@
+# Here we store multiple items of the same type together
+
+# we can create array using array module, all the elements of the array must be of the same type.
+
+# In python we can create an array by importing array module. array(data_type, value_list) is used to create an array with data type and value list specified in its arguments.
+
+import array as arr
+
+a= arr.array('b',[1,2,3])
+print("the newely created array is :", end=" ")
+for i in range (0,3):
+    print(a[i],end=' ')
+print()
+
+#a= arr.array('b',[1,2,3])
+# print("the newely created array is :", end=" ")
+# for i in range (0,3):
+#     print(a[i],end=' ')
+# print()                                                                   output 123 will be in a single line 
+
+
+# a= arr.array('b',[1,2,3])
+# print("the newely created array is :")
+# for i in range (0,3):
+#     print(a[i],end=' ')
+# print()                                                                   output 123 will be in the nxt line
+
+
+# a= arr.array('b',[1,2,3])
+# print("the newely created array is :", end=" ")
+# for i in range (0,3):
+#     print(a[i],end=' ')
+# print()                                                                   all the output 123 will be written in new line
+
