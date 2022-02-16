9a70d12638b1ab406a0ad27ae42a3b1534a6951d
AMRUTH MANDAPPA T.S <78676871+Amruth56@users.noreply.github.com>
2022-02-16 05:54:23 +0530

    Add files via upload

---
+++ 6_dictinory.py
@@ -0,0 +1,4 @@
+# its unordered collection of data values . Dictionary holds KEY:VALUE pair
+            # --- key in dictionary does not allow polymorphism 
+
+#--- Dictionary keys are case sensitive, the same name but different case of key will be  treated distinctly.
