1a5153ead7fd6792325785b298284494e6723aa0
Amruth <ammumandappa@gmail.com>
2022-03-04 00:30:54 +0530

    .

---
+++ variables/1_functions.py
@@ -0,0 +1,10 @@
+# its declared using "DEF" keyword before the name of the function 
+
+def division(a,b):
+    if a%b==0:
+        print("a is divisible by b")
+    else:
+        print("a is not divisible by b")
+division(4,2)
+
+print(type(type(int)))
---
+++ variables/2_global_n_local_var.py
@@ -0,0 +1,2 @@
+#Global variables are those which are not defined inside any functions and have a global scope 
+#Local variables are those which are defined inside a function and its scope is limited to that function only.
