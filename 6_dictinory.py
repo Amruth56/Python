cf4a877e51808a5d855c14240a61884966a524c1
Amruth <ammumandappa@gmail.com>
2022-03-03 18:53:31 +0530

    .

--- 6_dictinory.py
+++ 6_dictinory.py
@@ -1,7 +1,7 @@
 # its unordered collection of data values . Dictionary holds KEY:VALUE pair
             # --- key in dictionary does not allow polymorphism 
 
-#--- Dictionary keys are case sensitive, the same name but different case of key will be  treated distinctly.
+#----> Dictionary keys are case sensitive, the same name but different case of key will be  treated distinctly.
 
 #----> key:value     value in the dictionary can be of any data type and can be duplicate. where as keys must be immutable.
 
@@ -15,7 +15,7 @@ print(f"\nIts an empty dictionary `{dict}")
 
 #Adding elements to the empty dictionary
 dict[0]="hello"
-dict[1]=1
+dict[1]="one"
 dict[2]=2
 dict[3]=3
 dict[4]="mic testing"
@@ -26,7 +26,7 @@ print(f"\nThe dictionary after adding 5 mixed elements is     {dict}")
 #                                       Accessing elements from the list
 #--- in order to access the elements from a dictionary we make use of its key. Key can be used inside square brackets
 dict={1:"hello", 2:"world", "smile":"good", 6:"morning"}
-print(f"\nAccessing the elements from the list \t{dict[1]} {dict['smile']}")
+print(f"\nAccessing the elements from the list \t{dict[1]} \t{dict['smile']}")
 
 #we can also make use of get() method.
 print(f"Accessing the element from the list using get method \t{dict.get(6)}")
@@ -37,13 +37,13 @@ dict={'dict1':{1:"hello", 2:[11,22,33,44,55]},
     'dict2':{'position':'king', 'kingdom':'SriLanka', 'name':'Ravana'},
     'dict3':["q","w","e","r","t","y"]}
 
-print(f"\nAccessing elements from a nested dictionary \t{dict['dict1']}\t {dict['dict2']['name']}\t {dict['dict3'][0]}\t {dict['dict1'][2][0]}")
+print(f"\nAccessing elements from a nested dictionary \t{dict['dict1']}\n {dict['dict2']['name']}\n {dict['dict3'][0]}\n {dict['dict1'][2][0]}")
 
 
 
 #                                       Removing elements from the dictionary
 
-#--- most of the times deletion of key is done by the use of del keyword. Using the del keyword, specific values from the dictionary as well as the whole dictionary can be deleted. Items in a nested dictionary can also be deleyed by using the del keyword and providing a specific a specific nested key and particular key to be deleted from the nested Dictionary. 
+#---> most of the times deletion of key is done by the use of del keyword. Using the del keyword, specific values from the dictionary as well as the whole dictionary can be deleted. Items in a nested dictionary can also be deleted by using the del keyword and providing a specific nested key and particular key to be deleted from the nested Dictionary. 
 
 dict = {1:"hello",2:"world", 3:69, 5:[1,2,3,4,5], 'dict1':{'position':'king', 'kingdom':'SriLanka', 'name':'Ravana'}}
 print(f"\nInitial dictionary \t{dict}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
-set1=set()
-print(set1)
-set1.add(1)
-print(set1)
+dict = {1:"hello",2:"world", 3:69, 5:[1,2,3,4,5], 'dict1':{'position':'king', 'kingdom':'SriLanka', 'name':'Ravana'}}
+a=dict.pop(1)
+print(f"\nThe dictionary after poping a key element \t{dict}")
+print(f"The value associated vit the pop keyword is \t{a}")
