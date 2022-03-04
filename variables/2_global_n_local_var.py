df98f6d543e42c59789b4c34be2df5a8c82d985c
Amruth <ammumandappa@gmail.com>
2022-03-04 23:16:33 +0530

    Update 2_global_n_local_var.py

--- variables/2_global_n_local_var.py
+++ variables/2_global_n_local_var.py
@@ -1,2 +1,3 @@
 #Global variables are those which are not defined inside any functions and have a global scope 
-#Local variables are those which are defined inside a function and its scope is limited to that function only.
+#Local variables are those which are defined inside a function and its scope is limited to that function only.  
+# local variables are accessible only inside the function in which it was initialized where as the global variables are accessible throughout the program and inside every function. 
