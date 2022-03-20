a1a0210ffe0a8b3ee3af4fa386cd5fe95fa09014
Amruth <ammumandappa@gmail.com>
2022-03-20 09:05:47 +0530

    code

--- variables/tempCodeRunnerFile.py
+++ variables/tempCodeRunnerFile.py
@@ -1,11 +1,13 @@
 def f():
     global s
     s += "gfg"
-    print(s)
+    print("1",s)
+    s = "gfg"
+    print("1.1",s)
     s= "where are you now"
-    print(s)
+    print("2",s)
 
 #Global variable
-s= "King of court"
+s= "King of court "
 f()
-print(s)
+print("3",s)
