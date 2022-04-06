da9bf57cf3a48aef58f3ab26cb6e0387589b9ac4
Amruth <ammumandappa@gmail.com>
2022-04-06 10:53:21 +0530

    .

---
+++ variables/6_operator_overloading.py
@@ -0,0 +1,10 @@
+#it means giving extended meaning beyond their predefined operational meaning 
+
+# for example operator'+' adds two integer and joins two strings, it performs multiple functions
+#           THE SAME BUILT-IN OPERATOR OR FUNCTIONS SHOWS DIFFERENT BEHAVIOR FOR OBJECTS OF DIFF CLASSES, THIS IS CALLED AS OPERATOR OVERLOADING
+
+print(3+4)
+print("hello" +" world")
+
+print(2*2)
+print("hi "*4)
