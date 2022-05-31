27889ec58070c4db10c698ad823d22da0d9be806
Amruth <ammumandappa@gmail.com>
2022-05-31 20:00:06 +0530

    .

--- variables/08_operator_fns.py
+++ variables/08_operator_fns.py
@@ -9,3 +9,15 @@ print("multiplication of two numbers a and b is ", op.mul(a,b))
 print("power of two numbers a and b is ", op.pow(a,b))
 print("modulus of two numbers a and b is ", op.mod(a,b))
 
+
+li = [1,2,3,4,5,6]
+for i in range (0, len(li)):
+    print(li[i],end=" ")
+
+print("\n ")
+
+op.setitem(li,3,73)  # position, number
+for i in range (0, len(li)):
+    print(li[i],end=" ")
+
+# similarly for delitem, getitem
