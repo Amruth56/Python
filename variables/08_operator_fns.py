e4ee9c523d7dcb44e1caeed23ab9ffde926bbdb0
Amruth <ammumandappa@gmail.com>
2022-05-31 19:29:28 +0530

    operator

---
+++ variables/08_operator_fns.py
@@ -0,0 +1,11 @@
+import operator as op
+
+a=4
+b=3
+
+print("addition of two numbers a and b is ", op.add(a,b))
+print("subtraction of two numbers a and b is ", op.sub(a,b))
+print("multiplication of two numbers a and b is ", op.mul(a,b))
+print("power of two numbers a and b is ", op.pow(a,b))
+print("modulus of two numbers a and b is ", op.mod(a,b))
+
--- variables/tempCodeRunnerFile.py
+++ variables/tempCodeRunnerFile.py
@@ -1,19 +1,12 @@
-# ALL:
-
-# Illustration of 'all' function in python 3
-
-# Take two lists
-list1=[]
-list2=[]
-
-# All numbers in list1 are in form: 4*i-3
-for i in range(1,21):
-	list1.append(4*i-3)
-print("/n", list1)
-# list2 stores info of odd numbers in list1
-for i in range(0,20):
-	list2.append(list1[i]%2==1)
-print("/n",list2)
-
-print('See whether all numbers in list1 are odd =>')
-print(all(list2))
+# importing operator module
+import operator
+ 
+# Initializing list
+li = [1, 5, 6, 7, 8]
+ 
+# printing original list
+print ("The original list is : ",end="")
+for i in range(0,len(li)):
+    print (li[i],end=" ")
+ 
+print ("\r")
