e1a06719a3e6970ef91199f93dbd7933f09f0b56
Amruth <ammumandappa@gmail.com>
2022-05-31 07:25:38 +0530

    .

--- variables/04_tempCodeRunnerFile.py
+++ variables/04_tempCodeRunnerFile.py
@@ -1,4 +1,4 @@
 # ---------------------------------------------------------------------------------------
 a, b= 5,9
 #       [starement_on_true]   if [statement] else [statement_on_false]
-print( a,"is greaterthan b" if    (a>b)    else b,"b is greater than a " )
+print( "is greater than b" if    (a>b)    else "b is greater than a " )
